bd293bff17b8b086ce83ef6aec80a68519df5a82
liliankim <yejean831@gmail.com>
2022-05-31 23:03:46 -0700

    added if statement for timeblock

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 var day = moment().format('dddd, MMMM Do YYYY');
+
+var hoursNow = moment().hour();
+
+function background(){
+TimeBlocks.each(function()
+{
+    var hour = pareseInt($(this).attr("data-hour").split ('_')[0]);
+    console.log(hour)
+
+    if (hour<hoursNow){
+        $(this).addClass("past");
+    }
+    else if(hour === hoursNow){
+        $(this).addClass("present")
+    }
+    else{
+        $(this).addlass("future")
+    }
+});}
+
